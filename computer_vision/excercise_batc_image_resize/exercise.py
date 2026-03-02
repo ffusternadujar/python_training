74f24ac0bf94b3e7fa936c86bab41683c72f3aa6
Francisco Fuster Andujar <ffusterandujar@gmail.com>
2026-03-02 07:20:30 +0100

    feat: add computer vision batch file processor

---
+++ computer_vision/excercise_batc_image_resize/exercise.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+import glob
+
+def resize_images_in_folder(folder_path):
+  images = []
+  if not os.path.exists(folder_path):
+    print("Folder does not exist")
+    return
+  
+  images = glob.glob(os.path.join(folder_path, "*.jpg"))
+  if not images:
+    print("Foldr is empty.")
+    return
+
+  for image in images:
+    img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+    resized_img = cv2.resize(img, (100,100))
+    cv2.imwrite(os.path.join(folder_path, f"resized2_{os.path.basename(image)}.jpg"), resized_img)
+
+if __name__ == "__main__":
+  folder_path = "sample_images"
+  resize_images_in_folder(folder_path)
+  
