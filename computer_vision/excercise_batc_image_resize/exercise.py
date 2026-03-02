b553cddc02e2a5c8090f7104bb33459956c88990
Francisco Fuster Andujar <ffusterandujar@gmail.com>
2026-03-02 17:56:17 +0100

    refactor: improve code structure and formatting in image resizing function

--- computer_vision/excercise_batc_image_resize/exercise.py
+++ computer_vision/excercise_batc_image_resize/exercise.py
@@ -2,23 +2,29 @@ import os
 import cv2
 import glob
 
+
 def resize_images_in_folder(folder_path):
-  images = []
-  if not os.path.exists(folder_path):
-    print("Folder does not exist")
-    return
-  
-  images = glob.glob(os.path.join(folder_path, "*.jpg"))
-  if not images:
-    print("Foldr is empty.")
-    return
+    """Resizes all images in the specified folder to 100x100 pixels and saves them with a new name.
 
-  for image in images:
-    img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
-    resized_img = cv2.resize(img, (100,100))
-    cv2.imwrite(os.path.join(folder_path, f"resized2_{os.path.basename(image)}.jpg"), resized_img)
+    Args:        folder_path (str): The path to the folder containing the images to be resized.
+    Returns:        None
+    """
 
-if __name__ == "__main__":
-  folder_path = "sample_images"
-  resize_images_in_folder(folder_path)
-  
+    images = []
+    if not os.path.exists(folder_path):
+        print("Folder does not exist")
+        return
+
+    images = glob.glob(os.path.join(folder_path, "*.jpg"))
+    if not images:
+        print("Foldr is empty.")
+        return
+
+    for image in images:
+        img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+        resized_img = cv2.resize(img, (100,100))
+        cv2.imwrite(os.path.join(folder_path, f"resized2_{os.path.basename(image)}.jpg"), resized_img)
+
+    if __name__ == "__main__":
+        folder_path = "sample_images"
+        resize_images_in_folder(folder_path)
